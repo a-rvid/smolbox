2b19b573b1e4a1bbe8771860697243ea131a7988
a-rvid <git@rvid.eu>
2026-06-17 14:54:43 +0200

    some well needed formatting

--- src/main.c
+++ src/main.c
@@ -1,18 +1,16 @@
-#include<unistd.h>
-#include<libgen.h>
-#include<string.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
-#include<pwd.h>
-#include<sys/types.h>
+#include <libgen.h>
+#include <pwd.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define PRINT(string) write(1, string, strlen(string));
 #define PATH_MAX 4096
 
-int ls(int argc, char **argv, bool offset) {
-  return 0;
-}
+int ls(int argc, char **argv, bool offset) { return 0; }
 
 int pwd(int argc, char **argv, bool offset) {
   char cwd[PATH_MAX];
@@ -40,17 +38,18 @@ int whoami(int argc, char **argv, bool offset) {
 }
 
 typedef struct {
-    char* argument;
-    int (*handler)(int argc, char **argv, bool offset); // function returning int with void arguments
+  char *argument;
+  int (*handler)(int argc, char **argv,
+                 bool offset); // function returning int with void arguments
 } command;
 
-#define COMMANDS \
-  X("ls", ls) \
-  X("pwd", pwd) \
+#define COMMANDS                                                               \
+  X("ls", ls)                                                                  \
+  X("pwd", pwd)                                                                \
   X("whoami", whoami)
 
 #define X(argument, handler) {argument, handler},
-static const command commands[] = { COMMANDS };
+static const command commands[] = {COMMANDS};
 #undef X
 
 #define X(argument, handler) argument " "
@@ -59,23 +58,24 @@ static const char help[] = "[ " COMMANDS "]";
 
 constexpr size_t num_commands = sizeof(commands) / sizeof(command);
 
-int cmp(const void* a, const void* b) {
-    command* ca = (command*)a;
-    command* cb = (command*)b;
-    return strcmp(ca->argument, cb->argument);
+int cmp(const void *a, const void *b) {
+  command *ca = (command *)a;
+  command *cb = (command *)b;
+  return strcmp(ca->argument, cb->argument);
 }
 
 int main(int argc, char **argv) {
   for (int i = 0; i < 2; i++) {
     command key = {basename(argv[i]), NULL};
-    command *result = bsearch(&key, commands, num_commands, sizeof(command), cmp);
+    command *result =
+        bsearch(&key, commands, num_commands, sizeof(command), cmp);
 
     if (result == NULL) {
-      if (i == 1){
+      if (i == 1) {
         PRINT(help);
       }
     } else {
-        return result->handler(argc, argv, i);
+      return result->handler(argc, argv, i);
     }
   }
   return 1;
