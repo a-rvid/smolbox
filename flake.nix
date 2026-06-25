8131e0052e51318fef18732601f655bf9386e080
a-rvid <git@rvid.eu>
2026-06-25 23:45:01 +0200

    update flake.nix to make derivation

--- flake.nix
+++ flake.nix
@@ -3,15 +3,44 @@
    inputs = {
       nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
       flake-utils.url = "github:numtide/flake-utils";
+      linux = {
+          url = "git+https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git?shallow=1";
+          flake = false;
+      };
    };
 
-   outputs = { self, nixpkgs, flake-utils }:
+   outputs = { self, nixpkgs, flake-utils, linux }:
       flake-utils.lib.eachDefaultSystem (system:
          let
          pkgs = import nixpkgs {
             inherit system;
          };
          in {
+            packages.default = pkgs.stdenv.mkDerivation {
+                pname = "smolbox";
+                version = "0.1.0";
+                buildInputs = [
+                    pkgs.gcc
+                    pkgs.gnumake
+                    pkgs.pkg-config
+                ];
+
+
+                src = self;
+
+                buildPhase = ''
+                    rm -rf lib/linux
+                    cp -r ${linux} lib/linux
+                    chmod -R u+w lib/linux
+                    make
+                '';
+
+                installPhase = ''
+                    mkdir -p $out/bin
+                    cp smolbox $out/bin/
+                '';
+            };
+
             devShells.default = pkgs.mkShell {
                name = "dev";
 
@@ -23,7 +52,6 @@
                   gnumake
                   pkg-config
                   dpkg
-                  musl
                ];
 
                shellHook = ''
