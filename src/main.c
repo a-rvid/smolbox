81b928bcf8ebd1cb648a29e45e678b5e7f48c126
a-rvid <git@rvid.eu>
2026-06-25 23:57:39 +0200

    remove comments for placeholders in main and place in tools dir instead

--- src/main.c
+++ src/main.c
@@ -1,126 +1,13 @@
-/* #include <getopt.h> */
-/* #include <libgen.h> */
-/* #include <errno.h> */
-/* #include <pwd.h> */
 #define _NOLIBC_GETOPT_H
 #include <stdbool.h>
 #include <errno.h>
 #include <stdio.h>
 #include "tools.h"
-/* #include <stdlib.h> */
-/* #include <sys/stat.h> */
-/* #include <sys/types.h> */
-/* #include <sys/resource.h> */
 
 #define NAME "SmolBox"
 #define VERSION "v0.1.0"
 
 #define PATH_MAX 4096
-
-/* int ls(int argc, char **argv, bool offset) { */
-/*   char dirs[argc - 1 - offset]; */
-/*   bool longer = false, all = false, almost_all = false, human_readable = false; */
-
-/*   static struct option long_options[] = { */
-/*       {"long", no_argument, NULL, 'l'}, */
-/*       {"all", no_argument, NULL, 'a'}, */
-/*       {"almost-all", no_argument, NULL, 'A'}, */
-/*       {"human-readable", no_argument, NULL, 'h'}, */
-/*       {NULL, 0, NULL, 0}}; */
-/*   int opt; */
-/*   while ((opt = getopt_long(argc, argv, "laAh", long_options, NULL)) != -1) { */
-/*     switch (opt) { */
-/*     case 'l': */
-/*       longer = true; */
-/*       break; */
-/*     case 'a': */
-/*       all = true; */
-/*       break; */
-/*     case 'A': */
-/*       almost_all = true; */
-/*       break; */
-/*     case 'h': */
-/*       human_readable = true; */
-/*       break; */
-/*     } */
-/*   } */
-
-/*   /\* printf("Long: %d, all: %d, almost all: %d, human readable: %d\n", longer, */
-/*    * all, almost_all, human_readable); *\/ */
-
-/*   return 0; */
-/* } */
-
-/* int niceness(int argc, char **argv, bool offset) { */
-/*   errno = 0; */
-/*   int niceness = getpriority(PRIO_PROCESS, 0); */
-/*   if (errno != 0) { */
-/*     perror("getpriority"); */
-/*     return 1; */
-/*   } */
-
-/*   static struct option long_options[] = { */
-/*     {"adjustment", required_argument, NULL, 'n'}, */
-/*     {NULL, 0, NULL, 0} */
-/*   }; */
-
-/*   int outnice = niceness; */
-/*   int opt; */
-
-/*   char *fake_argv[3] = {NULL}; */
-/*   fake_argv[0] = argv[0]; */
-/*   fake_argv[1] = argv[1+offset]; */
-/*   // Segmentation Fault here!!! */
-/*   if ((opt = getopt_long(2, fake_argv, "n:", long_options, NULL)) == 'n') { */
-/*     outnice += atoi(optarg); */
-/*   } else { */
-/*     outnice += 10; */
-/*   } */
-
-/*   if (argc <= 1 + offset) { */
-/*     char sniceness[4]; */
-/*     Itoa(niceness, sniceness, 10); */
-/*     puts(sniceness); */
-/*     return 0; */
-/*   } */
-
-/*   if(setpriority(PRIO_PROCESS, 0, outnice) == -1) { */
-/*     perror("nice"); */
-/*     return 1; */
-/*   } else { */
-/*     if(execvp(argv[1 + offset], &argv[1 + offset]) == -1) { */
-/*       perror(argv[1 + offset]); */
-/*     }; */
-/*   } */
-
-/*   return 0; */
-/* } */
-
-/* int pwd(int argc, char **argv, bool offset) { */
-/*   char cwd[PATH_MAX]; */
-/*   if (getcwd(cwd, sizeof(cwd)) != NULL) { */
-/*     puts(cwd); */
-/*   } else { */
-/*     perror("getcwd error"); */
-/*     return 1; */
-/*   } */
-/*   return 0; */
-/* } */
-
-
-/* int whoami(int argc, char **argv, bool offset) { */
-/*   register struct passwd *pw; */
-/*   register uid_t uid; */
-
-/*   uid = geteuid(); */
-/*   pw = getpwuid(uid); */
-/*   if (pw) { */
-/*     puts(pw->pw_name); */
-/*     return 0; */
-/*   } */
-/*   /\* fprintf(stderr, "cannot find name for user ID %u", uid); *\/ */
-/*   return 1; */
-/* } */
 
 int cmp(const void *a, const void *b) {
   command *ca = (command *)a;
---
+++ src/tools/nice.h
@@ -0,0 +1,44 @@
+/* int niceness(int argc, char **argv, bool offset) { */
+/*   errno = 0; */
+/*   int niceness = getpriority(PRIO_PROCESS, 0); */
+/*   if (errno != 0) { */
+/*     perror("getpriority"); */
+/*     return 1; */
+/*   } */
+
+/*   static struct option long_options[] = { */
+/*     {"adjustment", required_argument, NULL, 'n'}, */
+/*     {NULL, 0, NULL, 0} */
+/*   }; */
+
+/*   int outnice = niceness; */
+/*   int opt; */
+
+/*   char *fake_argv[3] = {NULL}; */
+/*   fake_argv[0] = argv[0]; */
+/*   fake_argv[1] = argv[1+offset]; */
+/*   // Segmentation Fault here!!! */
+/*   if ((opt = getopt_long(2, fake_argv, "n:", long_options, NULL)) == 'n') { */
+/*     outnice += atoi(optarg); */
+/*   } else { */
+/*     outnice += 10; */
+/*   } */
+
+/*   if (argc <= 1 + offset) { */
+/*     char sniceness[4]; */
+/*     Itoa(niceness, sniceness, 10); */
+/*     puts(sniceness); */
+/*     return 0; */
+/*   } */
+
+/*   if(setpriority(PRIO_PROCESS, 0, outnice) == -1) { */
+/*     perror("nice"); */
+/*     return 1; */
+/*   } else { */
+/*     if(execvp(argv[1 + offset], &argv[1 + offset]) == -1) { */
+/*       perror(argv[1 + offset]); */
+/*     }; */
+/*   } */
+
+/*   return 0; */
+/* } */
