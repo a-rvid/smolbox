1c3e264f67dc5e3a355ff402d09fdf6e3f696820
a-rvid <git@rvid.eu>
2026-07-25 15:56:26 +0200

    fix perror warning

--- src/lib/others/perror.c
+++ src/lib/others/perror.c
@@ -1,13 +1,13 @@
 #include <string.h>
 #include <errno.h>
 
-void my_perror(const char *s) {
-  char *strerr = strerror(errno);
+void my_perror(char *s) {
+  const char *strerr = strerror(errno);
 
   struct iovec iov[3] = {
     {s, strlen(s)},
     {": ", 3},
-    {strerr, strlen(strerr)},
+    {(char *)strerr, strlen(strerr)},
   };
 
   writev(2, iov, 3);
