38731ad0535a47a42bc9aa8acb865f8880f3e042
a-rvid <git@rvid.eu>
2026-09-16 15:44:53 +0200

    complete echo command to a usable state (no escape sequences yet)

--- src/tools/echo.c
+++ src/tools/echo.c
@@ -6,9 +6,9 @@ int echo(int argc, char **argv) {
   bool interpret = false;
   int opt;
   opterr = 0;
+
   while ((opt = getopt(argc, argv, "neE")) != -1) {
     switch (opt) {
-
     case 'n':
       newline = false;
       break;
@@ -17,15 +17,43 @@ int echo(int argc, char **argv) {
       break;
     case 'E':
       break;
+    case '?':
+      optind--;
+      goto done;
     }
   }
-  for (int i = 1; i < argc; i++) {
-    fputs(argv[i], stdout);
+ done:
+
+
+  for (int i = optind; i < argc; i++) {
+    if (interpret) {
+      for (const char *p = argv[i]; *p; p++) {
+        if (*p == '\\' && p[1]) {
+          // TODO: add octal \x1b and color codes
+          switch (*(++p)) {
+          case 'n': putchar('\n'); break;
+          case 'r': putchar('\r'); break;
+          case 't': putchar('\t'); break;
+          case 'v': putchar('\v'); break;
+          case 'b': putchar('\b'); break;
+          case 'f': putchar('\f'); break;
+          case 'a': putchar('\a'); break;
+          case '\\': putchar('\\'); break;
+          default: putchar('\\'); putchar(*p); break;
+          }
+        } else {
+          putchar(*p);
+        }
+      }
+    } else {
+      fputs(argv[i], stdout);
+    }
     if (i != argc - 1) { putchar(' '); }
   }
 
-  if(newline == true) {
+  if(newline) {
     putchar('\n');
   }
+
   return 0;
 }
